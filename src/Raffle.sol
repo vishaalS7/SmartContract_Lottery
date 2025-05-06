fbe52ea4c49847b386669f7fae13a55944537b17
vishaalS7 <ivishaal1411@gmail.com>
2025-05-06 22:26:36 +0530

    first draft

---
+++ src/Raffle.sol
@@ -0,0 +1,129 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+import {VRFConsumerBaseV2Plus} from
+    "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
+import {VRFV2PlusClient} from "lib/chainlink-brownie-contracts/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
+
+/**
+ * @title Raffle
+ * @author @0xVishh
+ * @notice A simple raffle contract
+ * @dev imolements chainlink VRFv2.5
+ * @custom:experimental This is an experimental contract.
+ */
+contract Raffle is VRFConsumerBaseV2Plus {
+    error Raffle__SendMoreToEnterRaffle();
+    error Raffle__TransferFailed();
+    error Raffle__NotOpen();
+
+    enum RaffleState {
+        OPEN,
+        CALCULATING
+    }
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+    uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    bytes32 private immutable i_keyHash;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    address payable[] public s_players;
+    address private s_recentWinner;
+    /**
+     * @dev the duration of lottery in seconds
+     */
+    uint256 public s_lastTimeStamp;
+    RaffleState private s_raffleState;
+
+    event RaffleEnter(address indexed player);
+    event winnerPicked(address indexed winner);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gaslane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2Plus(vrfCoordinator) {
+        i_interval = interval;
+        i_entranceFee = entranceFee;
+        i_keyHash = gaslane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
+        s_raffleState = RaffleState.OPEN;
+    }
+
+    /**
+     * @dev This function allows users to enter the raffle by sending ETH
+     * @notice The entrance fee must be paid in order to enter the raffle
+     */
+    function enterRaffle() external payable {
+        // Logic to enter the raffle
+        // require (msg.value >= i_entranceFee, "Not enough ETH to enter the raffle");
+        // require (msg.value >= i_entranceFee, SendMoreToEnterRaffle()); // it should be used above 8.26 solidity verion
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
+        }
+        if (s_raffleState != RaffleState.OPEN) {
+            revert Raffle__NotOpen();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    // 1. get a random number 
+    // 2. use that random number to pick a winner
+    // 3. Be automatically called
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+        s_raffleState = RaffleState.CALCULATING;
+        // Getting a random number from chainlink
+        // Chainlink VRF { Verifiable Random Function }
+        // requestId = s_vrfCoordinator.requestRandomWords(
+        VRFV2PlusClient.RandomWordsRequest memory request = VRFV2PlusClient.RandomWordsRequest({
+            keyHash: i_keyHash,
+            subId: i_subscriptionId,
+            requestConfirmations: REQUEST_CONFIRMATIONS,
+            callbackGasLimit: i_callbackGasLimit,
+            numWords: NUM_WORDS,
+            extraArgs: VRFV2PlusClient._argsToBytes(
+                // Set nativePayment to true to pay for VRF requests with Sepolia ETH instead of LINK
+                VRFV2PlusClient.ExtraArgsV1({nativePayment: false})
+            )
+        });
+        uint256 requestId = s_vrfCoordinator.requestRandomWords(request);
+
+        // Logic to pick a winner
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] calldata randomWords) internal override {
+        uint256 indexOfWinner = randomWords[0] % s_players.length;
+        address payable recentWinner = s_players[indexOfWinner];
+        // Logic to transfer the prize to the winner
+        // Reset the players array for the next raffle
+        s_recentWinner = recentWinner;
+
+        s_raffleState = RaffleState.OPEN;
+        s_players = new address payable[](0);
+        s_lastTimeStamp = block.timestamp;
+        (bool success, ) = recentWinner.call{value: address(this).balance}("");
+        if (!success) {
+            revert Raffle__TransferFailed();
+        }
+        emit winnerPicked(s_recentWinner);
+    }
+
+    /**
+     * getter function
+     */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+}
